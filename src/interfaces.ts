8b9f4ea4297de1267866fb5a1a19c12e7129944b
gustavobarsan <gustavobarsan@gmail.com>
2022-02-05 19:12:30 -0300

    initial Users Class structure

---
+++ src/interfaces.ts
@@ -0,0 +1,6 @@
+export interface IUsers {
+  name: string;
+  email: string;
+  cpf: string;
+  tel: string;
+}
--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -1,5 +1,24 @@
 import db from "../db";
+import { IUsers } from "../interfaces";
 
-class Users {
+class Users implements IUsers {
+  name: string;
+  email: string;
+  cpf: string;
+  tel: string;
   
+  constructor(name: string, email: string, cpf: string, tel: string) {
+    this.name = name;
+    this.email = email;
+    this.cpf = cpf;
+    this.tel = tel;
+  }
+
+  async createUser () {
+    const cursor = db.db();
+    cursor.collection("users").createIndexes()
+  }
+
 }
+
+export default Users;
